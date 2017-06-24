4b6ed5058555a98903c3c0909fb9fa2bf327efd4
furio <francesco@furio.me>
2017-06-24 15:55:26 +0200

    Added serial for comparison

--- tr-reduction.jl
+++ tr-reduction.jl
@@ -1,4 +1,4 @@
-@everywhere function mark_graph(graph, mark, k, islice, jslice)
+@everywhere function mark_graph!(graph, mark, k, islice, jslice)
     for i in islice, j in jslice
         gs = min(graph[i,k],graph[k,j])
         if graph[i,j] <= gs
@@ -8,7 +8,7 @@
     end
 end
 
-@everywhere function change_graph(graph, mark, islice, jslice)
+@everywhere function change_graph!(graph, mark, islice, jslice)
     for i in islice, j in jslice
         if mark[i,j]
             graph[i,j] = 0
@@ -16,13 +16,13 @@ end
     end
 end
 
-@everywhere function start_kernel_mark(graph, mark, k)
-    mark_graph(graph, mark, k, 1:size(graph,1), 1:size(graph,2))
+@everywhere function start_kernel_mark!(graph, mark, k)
+    mark_graph!(graph, mark, k, 1:size(graph,1), 1:size(graph,2))
 end
 
-@everywhere function start_kernel_change(graph, mark, interval_split)
+@everywhere function start_kernel_change!(graph, mark, interval_split)
     idx = indexpids(graph)
-    change_graph(graph, mark, interval_split[idx]+1:interval_split[idx+1], 1:size(graph,2))
+    change_graph!(graph, mark, interval_split[idx]+1:interval_split[idx+1], 1:size(graph,2))
 end
 
 function transitive_reduction_parallel(graphin::Array{Int8,2})
@@ -34,7 +34,7 @@ function transitive_reduction_parallel(graphin::Array{Int8,2})
     @sync begin
         procs_list = procs(graph)
         for k in 1:graphsize[1]
-            @async remotecall_wait(start_kernel_mark, procs_list[(k%length(procs_list))+1], graph, markedgraph, k)
+            @async remotecall_wait(start_kernel_mark!, procs_list[(k%length(procs_list))+1], graph, markedgraph, k)
         end
     end 
 
@@ -42,11 +42,27 @@ function transitive_reduction_parallel(graphin::Array{Int8,2})
         procs_list = procs(graph)
         interval_split = [round(Int, s) for s in linspace(0,size(graph,1),length(procs_list)+1)]
         for p in procs_list
-            @async remotecall_wait(start_kernel_change, p, graph, markedgraph, interval_split)
+            @async remotecall_wait(start_kernel_change!, p, graph, markedgraph, interval_split)
         end
     end 
 
     return graph
+end
+
+function transitive_reduction_serial(graphin::Array{Int8,2})
+    graph = Array{Int8,2}(copy(graphin))
+    n = size(graphin,1)
+    for j in 1:n
+        for i in 1:n
+            if graph[i, j] > 0
+                for k in 1:n
+                    if graph[j, k] > 0
+                        graph[i, k] = 0
+                    end
+                end
+            end
+        end
+    end
 end
 
 containment_graph_little = Array{Int8,2}([
@@ -67,6 +83,7 @@ containment_graph = Array{Int8,2}([
       [0 0 0 0 0 0 1 0]])
 
 # Run once to warm up the GC
+transitive_reduction_serial(ones(Int8,10,10))
 transitive_reduction_parallel(ones(Int8,10,10))
 
 # Test
@@ -75,7 +92,12 @@ transitive_reduction_parallel(ones(Int8,10,10))
 @time res3 = transitive_reduction_parallel(ones(Int8,10,10))
 @time res4 = transitive_reduction_parallel(ones(Int8,100,100))
 @time res5 = transitive_reduction_parallel(ones(Int8,1000,1000))
-#@time res6 = transitive_reduction_parallel(ones(Int8,10000,10000))
+
+@time sres1 = transitive_reduction_serial(containment_graph_little)
+@time sres2 = transitive_reduction_serial(containment_graph)
+@time sres3 = transitive_reduction_serial(ones(Int8,10,10))
+@time sres4 = transitive_reduction_serial(ones(Int8,100,100))
+@time sres5 = transitive_reduction_serial(ones(Int8,1000,1000))
 
 # Verify results
 println(res1)
